d3163ea430fbb3f01774a10ceddb03c99e2915ea
macarthur <macarthur@laptop.home>
2012-11-17 22:37:30 -0500

    First official commit, the software should now be ready to use. It is currently version 1.0 and seems to work for me on most(if not all) sites.

---
+++ passgen_bookmarklet.js
@@ -0,0 +1,38 @@
+function generate_pass(){
+    var objs=document.getElementsByTagName('input');
+    var objs_len=objs.length;
+    var type='';
+    var j=0;
+    var tmp='';
+    var inputs=[];
+    var username='';
+    for(i=0;i<objs_len;++i){
+        type=objs[i].type;
+        if(type=='password'){
+            inputs[j++]=i;
+            tmp=objs[i].value;
+            if(tmp!==''){
+                password=tmp;
+            }
+        }
+    }
+    var inputs_len=inputs.length;
+    for(i=0;i<inputs_len;++i){
+        tmp=objs[inputs[i]-1].value;
+        if(tmp!==''){
+            username=tmp;
+        }
+    }
+    var salt=generate_salt(password,username);
+    salt=salt.substr(0,22);
+    salt='$2a$04$'+salt;
+    password=bcrypt(password,salt);
+    password=sha1(password);
+    password=hex_decode(password);
+    password=base32_encode(password);
+    password=password.substr(0,12);
+    for(i=0;i<inputs_len;++i){
+        objs[inputs[i]].value=password;
+    }
+}
+generate_pass();
