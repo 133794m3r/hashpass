46a25478a695b1a789b5d1c264906469ca932adc
macarthur <133794m3r@gmail.com>
2017-03-10 13:25:55 -0500

    added the testing utility for benchmarking hashes. Also now autocaps first character if it is a letter to make the generation exactly the same across all platforms even if the person does specifically make it a lowercase this will make it so that they can literally have the same output across devices it'll also autocapitlize the first characterof username/password too I think.

--- math.js
+++ math.js
@@ -270,3 +270,18 @@ function xor_rand(min,max){
 function microtime(){
     return (Date.now()/1000);
 }
+
+function display_time(seconds) {
+    var base, century, day, display_num, display_str, hour, minute, month, ref, year;
+    minute = 60;
+    hour = minute * 60;
+    day = hour * 24;
+    month = day * 31;
+    year = month * 12;
+    century = year * 100;
+    ref = seconds < 1 ? [null, 'less than a second'] : seconds < minute ? (base = Math.round(seconds), [base, base + " second"]) : seconds < hour ? (base = Math.round(seconds / minute), [base, base + " minute"]) : seconds < day ? (base = Math.round(seconds / hour), [base, base + " hour"]) : seconds < month ? (base = Math.round(seconds / day), [base, base + " day"]) : seconds < year ? (base = Math.round(seconds / month), [base, base + " month"]) : seconds < century ? (base = Math.round(seconds / year), [base, base + " year"]) : [null, 'centuries'], display_num = ref[0], display_str = ref[1];
+    if ((display_num != null) && display_num !== 1) {
+      display_str += 's';
+    }
+    return display_str;
+  }
--- salt_test.js
+++ salt_test.js
@@ -1,41 +1,86 @@
 function generate_salt(password,username,url){
-	var time=Date.now();
+	var time=microtime()
     var salt='';
 
     scrypt(password,url,{
-        logN:8,
+        logN:7,
         r:6,
-        p:2,
+        p:1,
         encoding:'base64'},
         function(x){password=x;}
     )
 
     scrypt(username,password,{
-        logN:8,
+        logN:7,
         r:6,
-        p:2,
+        p:1,
         encoding:'hex'},
         function(x){username=x;}
     )
 
     scrypt(url,username,{
-        logN:8,
+        logN:7,
         r:6,
-        p:2,
+        p:1,
         encoding:'base64'},
         function(x){url=x;}
     )
     salt=url+password+username;
     salt2=username+url+password;
     scrypt(salt,salt2,{
-        logN:10,
+        logN:9,
         r:8,
-        p:2,
+        p:1,
         encoding:'hex'},
         function(x){salt=x;}
     )    
-	var time2=Date.now();
-	console.log('gen_salt:'+(time2-time)+'ms');
-    console.log(salt);
+	var time2=microtime()
     return salt;
+}
+function do_test(num){
+var times=[];
+var urls=[];
+var usernames=[];
+var passwords=[];
+var tmp='';
+var start=0;
+var end=0;
+var true_start=microtime()
+var arr_start=1+(Math.floor(num/20));
+var arr_end=-1*arr_start;
+for(i=0;i<=num;i++){
+	urls[i]=((Math.random()).toString(16))+((Date.now()).toString(16));
+	usernames[i]=((Math.random()).toString(16))+((Date.now()).toString(16));
+	passwords[i]=((Math.random()).toString(16))+((Date.now()).toString(16));
+}
+
+for(i=0;i<=num;i++){
+	start=microtime()
+	tmp=generate_salt(passwords[i],usernames[i],urls[i]);
+	end=microtime()
+	times[i]=(end-start);
+}
+times=times.slice(arr_start,arr_end);
+salt_mean=mean(times);
+salt_stddev=standard_deviation(times,salt_mean);
+
+for(i=0;i<=num;i++){
+	start=microtime()
+	tmp=generate_salt(passwords[i],usernames[i],urls[i]);
+	scrypt(passwords[i],tmp,{
+            logN:15,
+            r:10,
+            p:1
+        },
+        function (x){tmp=x;}
+        );
+	end=microtime()
+	times[i]=(end-start);
+}
+//times=times.slice(arr_start,arr_end);
+true_mean=mean(times);
+true_stddev=standard_deviation(times,true_mean);
+var true_end=microtime()
+console.log('true time:'+(true_end-true_start));
+
 }
