7972b41c31e4aa81809f3575ae02f9445c396ac6
macarthur <133794m3r@gmail.com>
2018-04-12 20:59:27 -0400

    various sha256 implementations added. Rewrote old sha256 one as I don't want to rely on asm.js yet as that's a whole can of worms I don't want to deal with as of yet. Rewrote it to a faster version. ~6% faster than "fast" version and also ~3x as fast as the old fast version of sha256.
    This should help users using weaker computers to get their passwords much
    faster when using the scrypt function.
    
    TODO: finish unrolling looops in hashblocks and finish cleaning it up.

---
+++ sha_test/sha1.js
@@ -0,0 +1,213 @@
+function SHA256(m) {
+"use strict";
+    /** @const */ var K = [
+      0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b,
+      0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01,
+      0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7,
+      0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+      0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152,
+      0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+      0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc,
+      0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+      0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819,
+      0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08,
+      0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f,
+      0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+      0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    ];
+    var h0 = 0x6a09e667, h1 = 0xbb67ae85, h2 = 0x3c6ef372, h3 = 0xa54ff53a,
+        h4 = 0x510e527f, h5 = 0x9b05688c, h6 = 0x1f83d9ab, h7 = 0x5be0cd19,
+        w = new Array(64);
+    /*
+    if(Int32Array){
+      K=new Int32Array(K);
+      w=new Int32Array(w);
+    }
+*/
+    function blocks(p,p_len) {
+      var off = 0, len = p_len
+      while (len >= 64) {
+        var a = h0, b = h1, c = h2, d = h3, e = h4, f = h5, g = h6, h = h7,
+            u, i, j, t1, t2;
+
+        for (i = 0; i < 16; i++) {
+          j = off + i*4;
+          w[i] = ((p[j] & 0xff)<<24) | ((p[j+1] & 0xff)<<16) |
+                 ((p[j+2] & 0xff)<<8) | (p[j+3] & 0xff);
+        }
+
+        for (i = 16; i < 64; i++) {
+          u = w[i-2];
+          t1 = ((u>>>17) | (u<<(32-17))) ^ ((u>>>19) | (u<<(32-19))) ^ (u>>>10);
+
+          u = w[i-15];
+          t2 = ((u>>>7) | (u<<(32-7))) ^ ((u>>>18) | (u<<(32-18))) ^ (u>>>3);
+
+          w[i] = (((t1 + w[i-7]) | 0) + ((t2 + w[i-16]) | 0)) | 0;
+        }
+
+        for (i = 0; i < 64; i++) {
+          t1 = ((((((e>>>6) | (e<<(32-6))) ^ ((e>>>11) | (e<<(32-11))) ^
+               ((e>>>25) | (e<<(32-25)))) + ((e & f) ^ (~e & g))) | 0) +
+               ((h + ((K[i] + w[i]) | 0)) | 0)) | 0;
+
+          t2 = ((((a>>>2) | (a<<(32-2))) ^ ((a>>>13) | (a<<(32-13))) ^
+               ((a>>>22) | (a<<(32-22)))) + ((a & b) ^ (a & c) ^ (b & c))) | 0;
+
+          h = g;
+          g = f;
+          f = e;
+          e = (d + t1) | 0;
+          d = c;
+          c = b;
+          b = a;
+          a = (t1 + t2) | 0;
+        }
+
+        h0 = (h0 + a) | 0;
+        h1 = (h1 + b) | 0;
+        h2 = (h2 + c) | 0;
+        h3 = (h3 + d) | 0;
+        h4 = (h4 + e) | 0;
+        h5 = (h5 + f) | 0;
+        h6 = (h6 + g) | 0;
+        h7 = (h7 + h) | 0;
+
+        off += 64;
+        len -= 64;
+      }
+    }
+    var m_len=m.length;
+    blocks(m,m_len);
+
+    var i, bytesLeft = m_len % 64,
+        bitLenHi = (m_len / 0x20000000) | 0,
+        bitLenLo = m_len << 3,
+        numZeros = (bytesLeft < 56) ? 56 : 120,
+        p = m.slice(m_len - bytesLeft, m_len);
+  var p_len=p.length;
+
+    p[p_len++]=0x80;
+    for (i = bytesLeft + 1; i < numZeros; i++) p[p_len++]=0;
+    p[p_len++]=((bitLenHi>>>24) & 0xff);
+    p[p_len++]=((bitLenHi>>>16) & 0xff);
+    p[p_len++]=((bitLenHi>>>8)  & 0xff);
+    p[p_len++]=((bitLenHi>>>0)  & 0xff);
+    p[p_len++]=((bitLenLo>>>24) & 0xff);
+    p[p_len++]=((bitLenLo>>>16) & 0xff);
+    p[p_len++]=((bitLenLo>>>8)  & 0xff);
+    p[p_len++]=((bitLenLo>>>0)  & 0xff);
+
+    blocks(p,p_len);
+
+    return [
+      (h0>>>24) & 0xff, (h0>>>16) & 0xff, (h0>>>8) & 0xff, (h0>>>0) & 0xff,
+      (h1>>>24) & 0xff, (h1>>>16) & 0xff, (h1>>>8) & 0xff, (h1>>>0) & 0xff,
+      (h2>>>24) & 0xff, (h2>>>16) & 0xff, (h2>>>8) & 0xff, (h2>>>0) & 0xff,
+      (h3>>>24) & 0xff, (h3>>>16) & 0xff, (h3>>>8) & 0xff, (h3>>>0) & 0xff,
+      (h4>>>24) & 0xff, (h4>>>16) & 0xff, (h4>>>8) & 0xff, (h4>>>0) & 0xff,
+      (h5>>>24) & 0xff, (h5>>>16) & 0xff, (h5>>>8) & 0xff, (h5>>>0) & 0xff,
+      (h6>>>24) & 0xff, (h6>>>16) & 0xff, (h6>>>8) & 0xff, (h6>>>0) & 0xff,
+      (h7>>>24) & 0xff, (h7>>>16) & 0xff, (h7>>>8) & 0xff, (h7>>>0) & 0xff
+    ];
+  }
+function PBKDF2_HMAC_SHA256_OneIter(password, salt,dkLen) {
+"use strict";
+    // compress password if it's longer than hash block length
+    password = password.length <= 64 ? password : SHA256(password);
+
+    var i, innerLen = 64 + salt.length + 4,
+        inner = new Array(innerLen),
+        outerKey = new Array(64),
+        dk = [];
+    var password_len=password.length;
+    var salt_len=salt.length;
+    // inner = (password ^ ipad) || salt || counter
+    for (i = 0; i < 64; i++) inner[i] = 0x36;
+    for (i = 0; i < password_len; i++) inner[i] ^= password[i];
+    for (i = 0; i < salt_len; i++) inner[64+i] = salt[i];
+    for (i = innerLen - 4; i < innerLen; i++) inner[i] = 0;
+
+    // outerKey = password ^ opad
+    for (i = 0; i < 64; i++) outerKey[i] = 0x5c;
+    for (i = 0; i < password_len; i++) outerKey[i] ^= password[i];
+
+    // increments counter inside inner
+    function incrementCounter() {
+      for (var i = innerLen-1; i >= innerLen-4; i--) {
+        inner[i]++;
+        if (inner[i] <= 0xff) return;
+        inner[i] = 0;
+      }
+    }
+
+    // output blocks = SHA256(outerKey || SHA256(inner)) ...
+    while (dkLen >= 32) {
+      incrementCounter();
+      dk = dk.concat(SHA256(outerKey.concat(SHA256(inner))));
+      dkLen -= 32;
+    }
+    if (dkLen > 0) {
+      incrementCounter();
+      dk = dk.concat(SHA256(outerKey.concat(SHA256(inner))).slice(0, dkLen));
+    }
+    return dk;
+  }
+  function stringToUTF8Bytes(s) {
+"use strict";
+      var arr = [];
+      var s_len=s.length;
+    var arr_len=0;
+      for (var i = 0; i < s_len; i++) {
+          var c = s.charCodeAt(i);
+          if (c < 128) {
+              arr[arr_len++]=c;
+          } else if (c > 127 && c < 2048) {
+              arr[arr_len++]=((c>>6) | 192);
+              arr[arr_len++]=((c & 63) | 128);
+          } else {
+              arr[arr_len++]=((c>>12) | 224);
+              arr[arr_len++]=(((c>>6) & 63) | 128);
+              arr[arr_len++]=((c & 63) | 128);
+          }
+      }
+      return arr;
+  }
+
+
+  function bytesToHex(p) {
+"use strict";
+    /** @const */
+    var enc = '0123456789abcdef'.split('');
+
+    var len = p.length,
+        arr = [],
+        i = 0;
+
+    for (; i < len; i++) {
+        arr.push(enc[(p[i]>>>4) & 15]);
+        arr.push(enc[(p[i]>>>0) & 15]);
+    }
+    return arr.join('');
+  }
+
+function sha256(s){
+"use strict";
+    s=stringToUTF8Bytes(s);
+    var hash=SHA256(s);
+    hash=bytesToHex(hash);
+    return hash;
+}
+function pbkdf2_new(password,salt,rounds,dklen){
+"use strict";
+    var a='';
+    var i=0;
+    var pass=stringToUTF8Bytes(password);
+    var salts=stringToUTF8Bytes(salt);
+    dklen=dklen || 32
+    for(i=0;i<=rounds;i++){
+        a=PBKDF2_HMAC_SHA256_OneIter(pass,salts,dklen);
+    }
+    a=bytesToHex(a);
+    return a;
+}
