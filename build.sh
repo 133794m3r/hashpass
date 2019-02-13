7eb0bea176a07c2a9a114b9efce73b99d73c6bc2
macarthur inbody <133794m3r@gmail.com>
2019-02-13 15:30:34 -0500

    actually update things properly.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -jar compiler.jar --js=awesome_pass_gen2.js string.js math.js --js_output_file=awesome_pass_gen2-combined.min.js
+java -jar compiler.jar --js=awesome_pass_gen.js string.js math.js --js_output_file=awesome_pass_gen2-combined.min.js
 
 echo "Compiled javascript.\n";
 #php5 url_encode.php;
